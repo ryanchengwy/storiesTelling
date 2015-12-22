7e68378950b9895353134ce954e4c0748e95dd4f
RyanC <ryan.chengwy@gmail.com>
2015-12-22 10:38:06 +0800

    Initial Commit

---
+++ storiesTellingTests/storiesTellingTests.swift
@@ -0,0 +1,36 @@
+//
+//  storiesTellingTests.swift
+//  storiesTellingTests
+//
+//  Created by Ryan on 22/12/2015.
+//  Copyright © 2015 Ryan. All rights reserved.
+//
+
+import XCTest
+@testable import storiesTelling
+
+class storiesTellingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
