e46c7031990d3102c24e06ea89e769ec425b90d0
RyanC <ryan.chengwy@gmail.com>
2016-01-20 20:16:05 +0800

    refresh

--- storiesTelling/StorySummaryTableViewController.swift
+++ storiesTelling/StorySummaryTableViewController.swift
@@ -1,8 +1,8 @@
 //
-//  StorySummaryViewController.swift
+//  StorySummaryTableViewController.swift
 //  storiesTelling
 //
-//  Created by Ryan on 6/1/2016.
+//  Created by Ryan on 20/1/2016.
 //  Copyright © 2016 Ryan. All rights reserved.
 //
 
@@ -11,12 +11,9 @@ import Alamofire
 import SwiftyJSON
 import YYWebImage
 
+class StorySummaryTableViewController: UITableViewController {
 
-
-class StorySummaryViewController: UITableViewController {
-
-    @IBOutlet weak var storySummaryTable: UITableView!
-    
+    @IBOutlet var storySummaryTable: UITableView!
     let apiUrl = "http://139.162.22.162/api/v1/chapters/finished"
     var currentPage = 1
     var finishedStoryArray = [Story]()
@@ -24,48 +21,50 @@ class StorySummaryViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         if refreshControl == nil {
             
-        refreshControl = UIRefreshControl()
+            refreshControl = UIRefreshControl()
         }
-
+        refreshControl?.attributedTitle = NSAttributedString(string: "Refreshing")
+        refreshControl?.addTarget(self, action: "getDataFromServer:", forControlEvents: .ValueChanged)
+        
         //Auto height for the cell
         self.storySummaryTable.rowHeight = UITableViewAutomaticDimension
         self.storySummaryTable.estimatedRowHeight = 50
-
+        
         self.tabBarController?.tabBar.hidden = false
-          tabBarController?.tabBar.tintColor = UIColor.orangeColor()
+        tabBarController?.tabBar.tintColor = UIColor.orangeColor()
         storySummaryTable.dataSource = self
         storySummaryTable.delegate = self
         
         self.getDataFromServer(self.refreshControl!)
     }
     
-    func getDataFromServer (sender: AnyObject) {
+    func getDataFromServer(sender: AnyObject) {
         
         let apiPath = apiUrl
         let param = ["page": currentPage]
         
         Alamofire.request(.GET, apiPath, parameters: param).responseJSON { response in switch response.result {
-            case .Success(let data):
-                
+        case .Success(let data):
+            
             let result = JSON(data)["stories"]
             
             for(_, subJson):(String, JSON) in result {
-            let story = Story(json: subJson)
-            
-            self.finishedStoryArray.append(story)
-            
-            self.storySummaryTable.reloadData()
+                let story = Story(json: subJson)
                 
-            
+                self.finishedStoryArray.append(story)
+                self.storySummaryTable.reloadData()
+                
+                
+                
                 
             }
-     
+            self.refreshControl?.endRefreshing()
             
             
-            case .Failure(let error):
+        case .Failure(let error):
             print("\(error)")
             }
         }
@@ -110,60 +109,60 @@ class StorySummaryViewController: UITableViewController {
                 cell.storyCoverImage.image = UIImage(named: "\(randomPic)")
             }
         } else {
-        let parentArray = story.parent
-        
-        if parentArray!.count == 1{
-            cell.labelTitlePart1.text = story.parent![0].topic
-            cell.labelStorySetting.text = story.parent![0].setting
-            cell.labelTitlePart2.text = ""
-            cell.labelTitlePart3.text = story.topic
-            if story.avatar?.url != ""{
-                cell.storyCoverImage.yy_setImageWithURL(NSURL(string: (story.avatar?.url)!), options: .SetImageWithFadeAnimation)
+            let parentArray = story.parent
+            
+            if parentArray!.count == 1{
+                cell.labelTitlePart1.text = story.parent![0].topic
+                cell.labelStorySetting.text = story.parent![0].setting
+                cell.labelTitlePart2.text = ""
+                cell.labelTitlePart3.text = story.topic
+                if story.avatar?.url != ""{
+                    cell.storyCoverImage.yy_setImageWithURL(NSURL(string: (story.avatar?.url)!), options: .SetImageWithFadeAnimation)
+                    
+                }else{
+                    let randomPic = Int(arc4random_uniform(7))
+                    cell.storyCoverImage.image = UIImage(named: "\(randomPic)")
+                }
                 
-            }else{
-                let randomPic = Int(arc4random_uniform(7))
-                cell.storyCoverImage.image = UIImage(named: "\(randomPic)")
             }
-
-        }
-        if parentArray!.count == 2 {
-            cell.labelTitlePart1.text = story.parent![0].topic
-            cell.labelStorySetting.text = story.parent![0].setting
-            cell.labelTitlePart2.text = story.parent![1].topic
-            cell.labelTitlePart3.text = story.topic
-            if story.avatar?.url != ""{
-                cell.storyCoverImage.yy_setImageWithURL(NSURL(string: (story.avatar?.url)!), options: .SetImageWithFadeAnimation)
+            if parentArray!.count == 2 {
+                cell.labelTitlePart1.text = story.parent![0].topic
+                cell.labelStorySetting.text = story.parent![0].setting
+                cell.labelTitlePart2.text = story.parent![1].topic
+                cell.labelTitlePart3.text = story.topic
+                if story.avatar?.url != ""{
+                    cell.storyCoverImage.yy_setImageWithURL(NSURL(string: (story.avatar?.url)!), options: .SetImageWithFadeAnimation)
+                    
+                }else{
+                    let randomPic = Int(arc4random_uniform(7))
+                    cell.storyCoverImage.image = UIImage(named: "\(randomPic)")
+                }
                 
-            }else{
-                let randomPic = Int(arc4random_uniform(7))
-                cell.storyCoverImage.image = UIImage(named: "\(randomPic)")
             }
-
-        }
         }
         
-
-    
-
-
+        
+        
+        
+        
         
         return cell
     }
-
+    
     let randomPic = Int(arc4random_uniform(7))
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
         if segue.identifier == "summaryToDetial" {
-        
+            
             let vc = segue.destinationViewController as! StoryDetialViewController
             vc.story = self.finishedStoryArray[(self.storySummaryTable.indexPathForSelectedRow?.row)!]
             
-
+            
         }
         
         if let indexPath = self.storySummaryTable.indexPathForSelectedRow {
             storySummaryTable.deselectRowAtIndexPath(indexPath, animated: true)
         }
     }
-
+    
 }
